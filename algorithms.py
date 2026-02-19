46dd673c6febc3da77050840bd062b019a5a558b
harsij01 <harsinij6530@gmail.com>
2026-02-19 22:20:13 +0530

    added merge_sort

--- algorithms.py
+++ algorithms.py
@@ -33,3 +33,35 @@ def selection_sort(arr):
         arr[min_index], arr[i] = arr[i], arr[min_index]
 
     return arr
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+print(merge_sort([1, 6, 3, 29, 7, 30, 15, 2]))
