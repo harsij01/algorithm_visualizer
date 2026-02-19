c10417a0d0257565bffd3eb2af908cd67cdedcf9
harsij01 <harsinij6530@gmail.com>
2026-02-19 20:15:21 +0530

    added steps

--- algorithms.py
+++ algorithms.py
@@ -1,22 +1,20 @@
 def bubble_sort(arr):
     arr = arr.copy()
-
     n = len(arr)
-    comparisons = 0
-    swaps = 0
+    steps = []
 
     for i in range(n - 1):
         swapped = False
 
         for j in range(n - i - 1):
-            comparisons += 1
+            steps.append({'type': 'comparison', 'indices': (j, j + 1), 'array': arr.copy()})
 
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-                swaps += 1
+                steps.append({'type': 'step', 'indices': (j, j + 1), 'array': arr.copy()})
         
         if not swapped:
             break
 
-    return arr, comparisons, swaps
+    return arr, steps
