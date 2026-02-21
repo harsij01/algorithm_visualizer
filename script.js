9577ccb4a9c93a9aabd27c9b0d728079f61c97f6
harsij01 <harsinij6530@gmail.com>
2026-02-21 14:45:22 +0530

    added random array generation

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function sendArray() {
 
     const array = input.split(',').map(num => Number(num.trim())).filter(num => !isNaN(num))
 }
+
+function generateRandom() {
+    const arr = Array.from({length: 20}, () => Math.floor(Math.random() * 100));
+
+    document.getElementById("arrayInput").value = arr.join(",");
+}
