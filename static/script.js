006a7aafc0bb5ca082965984ef8eca9a2760d7b8
HarsiniJ <harsinij6530@gmail.com>
2026-02-28 05:58:30 +0530

    added tooltips

--- static/script.js
+++ static/script.js
@@ -351,3 +351,9 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initialize on page load
     updateModeUI();
 });
+
+// Enable all Bootstrap tooltips
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+tooltipTriggerList.map(function (el) {
+  return new bootstrap.Tooltip(el);
+});
